88fb192aa0eafb99d9a41ef5c48d087eb2094cfc
Taylor Brown <browtayl@onid.oregonstate.edu>
2016-03-06 20:30:04 -0800

    add add account page

---
+++ add_account.php
@@ -0,0 +1,18 @@
+<?php
+
+/* add_account.php
+ * add an account */
+
+/* shared functions */
+require 'financial_planning.php';
+
+/* display html page header */
+display_header();
+
+/* display add account form */
+print_file('add_account.html');
+
+/* display html page footer */
+display_footer();
+
+?>
