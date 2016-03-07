97d90a286cfdabe38351151b90a3dd0b44fe4a06
Taylor Brown <browtayl@onid.oregonstate.edu>
2016-03-06 19:42:40 -0800

    main application page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* index.php
+ * main application page */
+
+/* shared functions */
+require 'financial_planning.php';
+
+/* display html page header */
+display_header();
+
+/* placeholder main page */
+display_placeholder();
+
+/* display html page footer */
+display_footer();
+
+?>
