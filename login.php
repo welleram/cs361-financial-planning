e85d5e348594b6d2f805f2f5f35494f6e7db336a
Trent Richardson <wherestrent@gmail.com>
2016-03-09 20:02:52 -0700

    added alternate header to login/register to match index

--- login.php
+++ login.php
@@ -6,14 +6,14 @@
 /* shared functions */
 require 'financial_planning.php';
 /* display html page header */
-display_header();
+display_header_login();
 
 ?>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<script type="text/javascript" src="js/jquery.js"></script>
-<link rel="shortcut icon" href="../favicon.ico"> 
+<link rel="shortcut icon" href="../favicon.ico">
 	    <script src = "dropdown.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -24,8 +24,8 @@ display_header();
   z-index: 0;
 }
 .imageContainer {
-       width:200px; 
-       height:200px; 
+       width:200px;
+       height:200px;
        background-image: 3.pgn;
  }
 </style>
@@ -65,46 +65,46 @@ $(document).ready(function(){
     <!-- Image backgrounds are set within the full-width-pics.css file. -->
     <header class="image-bg-fluid-height">
         <img class="bg" src="3.png"  height="300" alt="">
-		
+
     </header>
 	<br>
 	<br>
 
 
-	
+
   <div class="container">
-            <section>				
+            <section>
                 <div id="container_demo" >
                     <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
                     <div id="wrapper">
-				
+
 			<form action="verify.php" class="form-horizontal">
 				<legend id="add_account_title"> <h1> Welcome Back </h1> </legend>
-								<div class="form-group" > 
+								<div class="form-group" >
                                     <label for="username" class="col-md-4 control-label" >Username: </label>
 									 <div class="col-md-4">
                                     <input id="username" name="username" required="required" type="text" placeholder="username" class="form-control input-md"/>
                                 </div>
 								</div>
-                                								
+
 								<div class="form-group">
 									<label for="password" class="col-md-4 control-label" >Password: </label>
                                      <div class="col-md-4">
 										<input id="password" name="password" required="required" type="text" placeholder="password" class="form-control input-md" />
 									</div>
 								</div>
-                           
-															
+
+
 								<div class="form-group">
 								 <label class="col-md-4 control-label" for="login_button"></label>
 								<div class="col-md-4">
 									<input type="submit" value="LOGIN" id="login_button" class="btn btn-primary" />
 								</div>
 								</div>
-								
-						
-							
-	
+
+
+
+
 								<br>
 								<font color="red"><div class="err" id="alert"></div></font>
 								<br>
@@ -112,19 +112,19 @@ $(document).ready(function(){
 									Not a member yet ?
 									<a href="register.php" class="to_register"><font size=4>Sign up </font></a>
 								</p>
-								
-				
-								
-								
-								
-								
+
+
+
+
+
+
 			</form>
-		
+
 			</div>
 	</div>
 	</section>
 	</div>
-	
+
 
 </body>
 <?php
